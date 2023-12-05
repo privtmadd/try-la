74bb4199f029805171d1b4b211bb5f68d51a4545
privtmadd <153012057+privtmadd@users.noreply.github.com>
2023-12-06 01:48:33 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Message for u:3</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="wrapper">
+		<h2 class="question">Do you like me?</h2>
+		<img class="gif" alt="gif" src="otter.gif">
+		<div class="btn-group"> <button class="yes-btn">Yes</button> <button class="no-btn">No</button></div>
+	</div>
+	<script src="script.js"></script>
+</body>
+</html>
